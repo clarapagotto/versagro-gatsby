ae093fbd6514f60b3f43b2604e6ffa9f11496cca
Lauro <lauro.floresta@gmail.com>
2020-07-01 18:42:20 -0300

    saco

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,7 @@ import SEO from "../components/seo"
 
 import SolutionsHome from "../components/Solutions"
 import DepoimentosHome from "../components/Depoimentos"
-<<<<<<< HEAD
 // import ContatoHome from "../components/Contato"
-=======
-import BlogHome from "../components/Blog"
->>>>>>> 34cf5922b14a3b2149f7d9224ac00b59ec0ea727
 
 import HomeSlider from "../components/HomeSlider"
 import { SliderWrapper } from "../components/HomeSlider/styled"
@@ -21,13 +17,8 @@ const IndexPage = () => (
       <HomeSlider />
     </SliderWrapper>
     <SolutionsHome />
-<<<<<<< HEAD
     <DepoimentosHome />
     {/* <ContatoHome /> */}
-=======
-    <DepoimentosHome /> 
-    <BlogHome />   
->>>>>>> 34cf5922b14a3b2149f7d9224ac00b59ec0ea727
   </Layout>
 )
 
