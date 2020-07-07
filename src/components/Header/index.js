7e72fdd01acb5c32d082bb4d8e7f18d81e5e9482
Lauro <lauro.floresta@gmail.com>
2020-07-06 22:15:26 -0300

    bug do botao 'A-Versagro' do header resolvido

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -33,113 +33,110 @@ function Header() {
           <Logo />
         </LogoLink>
         <MenuGroup>
-          <MenuItem>
-            <MenuLink to="/" activeClassName="active">
-              Início
-            </MenuLink>
-          </MenuItem>
-          <MenuItem>
-            <MenuLink to="/a-versagro" activeClassName="active" scroll={scroll}>
-              A Versagro
-            </MenuLink>
-          </MenuItem>
-          <MenuItem>
-            <MenuLink to="/solucoes" activeClassName="active">
-              Soluções
-            </MenuLink>
-          </MenuItem>
-          <MenuItem>
-            <MenuLink to="/cases" activeClassName="active">
-              Cases
-            </MenuLink>
-          </MenuItem>
-          <MenuItem>
-            <MenuLink to="/blog" activeClassName="active">
-              Blog
-            </MenuLink>
-          </MenuItem>
-          <MenuItem>
+          <MenuLink to="/" activeClassName="active">
+            Início
+          </MenuLink>
 
-            <MenuLink to="/contato" activeClassName="active">
-              Contato
-            </MenuLink>
-          </MenuItem>
+          <MenuLink to="/a-versagro" activeClassName="active" scroll={scroll}>
+            A Versagro
+          </MenuLink>
+
+          <MenuLink to="/solucoes" activeClassName="active">
+            Soluções
+          </MenuLink>
+
+          <MenuLink to="/cases" activeClassName="active">
+            Cases
+          </MenuLink>
+
+          <MenuLink to="/blog" activeClassName="active">
+            Blog
+          </MenuLink>
+
+          <MenuLink to="/contato" activeClassName="active">
+            Contato
+          </MenuLink>
         </MenuGroup>
       </MenuContainer>
     </MenuWrapper>
   )
-
-
 }
 //testtest
 
-
 export default Header
 
-// class Header extends Component {
-//   state = { scroll: 0 };
+// import React, { useEffect, useState } from "react"
+// import {
+//   MenuWrapper,
+//   MenuContainer,
+//   MenuGroup,
+//   MenuItem,
+//   MenuLink,
+//   LogoLink,
+// } from "./styled"
+// import Logo from "../Logo"
 
-//   componentDidMount() {
+// function Header() {
+//   const [scroll, setScroll] = useState(0)
+
+//   useEffect(() => {
 //     window.onscroll = () => {
-//       console.log('roi')
-//       this.handleScroll()
+//       handleScroll()
 //     }
-//   }
+//   }, [])
 
-//   handleScroll() {
+//   const handleScroll = () => {
 //     if (document.documentElement.scrollTop > 0) {
-//       this.setState({
-//         scroll: document.documentElement.scrollTop,
-//       })
+//       setScroll(document.documentElement.scrollTop)
 //     } else {
-//       this.setState({
-//         scroll: 0,
-//       })
+//       setScroll(0)
 //     }
 //   }
 
-//   render() {
-//     return (
-//       <MenuWrapper scroll={this.state.scroll}>
-//         <MenuContainer>
-//           <LogoLink to="/">
-//             <Logo />
-//           </LogoLink>
-//           <MenuGroup>
-//             <MenuItem>
-//               <MenuLink to="/" activeClassName="active">
-//                 Início
-//               </MenuLink>
-//             </MenuItem>
-//             <MenuItem>
-//               <MenuLink to="/a-versagro" activeClassName="active">
-//                 A Versagro
-//               </MenuLink>
-//             </MenuItem>
-//             <MenuItem>
-//               <MenuLink to="/solucoes" activeClassName="active">
-//                 Soluções
-//               </MenuLink>
-//             </MenuItem>
-//             <MenuItem>
-//               <MenuLink to="/cases" activeClassName="active">
-//                 Cases
-//               </MenuLink>
-//             </MenuItem>
-//             <MenuItem>
-//               <MenuLink to="/blog" activeClassName="active">
-//                 Blog
-//               </MenuLink>
-//             </MenuItem>
-//             <MenuItem>
-//               <MenuLink to="/contato" activeClassName="active">
-//                 Contato
-//               </MenuLink>
-//             </MenuItem>
-//           </MenuGroup>
-//         </MenuContainer>
-//       </MenuWrapper>
-//     )
-//   }
-// }
+//   return (
+//     <MenuWrapper scroll={scroll}>
+//       <MenuContainer>
+//         <LogoLink to="/">
+//           <Logo />
+//         </LogoLink>
+//         <MenuGroup>
+//           <MenuItem>
+//             <MenuLink to="/" activeClassName="active">
+//               Início
+//             </MenuLink>
+//           </MenuItem>
+//           <MenuItem>
+//             <MenuLink to="/a-versagro" activeClassName="active" scroll={scroll}>
+//               A Versagro
+//             </MenuLink>
+//           </MenuItem>
+//           <MenuItem>
+//             <MenuLink to="/solucoes" activeClassName="active">
+//               Soluções
+//             </MenuLink>
+//           </MenuItem>
+//           <MenuItem>
+//             <MenuLink to="/cases" activeClassName="active">
+//               Cases
+//             </MenuLink>
+//           </MenuItem>
+//           <MenuItem>
+//             <MenuLink to="/blog" activeClassName="active">
+//               Blog
+//             </MenuLink>
+//           </MenuItem>
+//           <MenuItem>
 
+//             <MenuLink to="/contato" activeClassName="active">
+//               Contato
+//             </MenuLink>
+//           </MenuItem>
+//         </MenuGroup>
+//       </MenuContainer>
+//     </MenuWrapper>
+//   )
+
+// }
+// //testtest
+
+// export default Header
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -33,30 +33,53 @@ export const MenuWrapper = styled.div`
 `
 
 export const MenuContainer = styled.div`
+  /* background-color: orange; */
+
   display: flex;
   justify-content: space-around;
   width: 75%;
   margin: 2px auto;
 `
 
-export const MenuGroup = styled.ul`
+export const LogoLink = styled(Link)`
+  /* background-color: yellow; */
+
+  padding-top: 40px;
+  width: 262px;
+  height: 77px;
+`
+
+export const MenuGroup = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: center;
 
-  /* background-color: blue;
-  flex: 1; */
+  padding: 0 2px;
 `
 
-export const MenuItem = styled.li`
-  padding: 0 2px 0 2px;
-  list-style: none;
+// export const MenuItem = styled.div`
+//   width: 120px;
+//   text-align: center;
 
-  /* background-color: pink;
-  border: 1px solid #fff; */
-`
+
+//   padding: 0 2px 0 2px;
+
+//   background-color: pink;
+//   border: 1px solid #fff;
+
+// `
 
 export const MenuLink = styled(Link)`
+  display: block;
+  /* background-color: pink;
+  border: 1px solid #fff; */
+
+  padding: 17px 0 10px;
+
+  width: 120px;
+  height: 28px;
+  text-align: center;
+
   font-family: "Raleway", sans-serif;
   font-weight: 300;
   font-size: 14px;
@@ -64,7 +87,9 @@ export const MenuLink = styled(Link)`
   text-align: center;
   letter-spacing: 1.5px;
 
-  padding: 18px 30px;
+  
+  /* padding: 18px 30px; */
+
   color: white;
   text-decoration: none;
 
@@ -79,8 +104,91 @@ export const MenuLink = styled(Link)`
   }
 `
 
-export const LogoLink = styled(Link)`
-  padding-top: 40px;
-  width: 262px;
-  height: 77px;
-`
+// import styled, { css } from "styled-components"
+// import { Link } from "gatsby"
+
+// export const MenuWrapper = styled.div`
+//   position: fixed;
+//   display: flex;
+//   align-items: center;
+//   z-index: 1;
+//   background-color: transparent;
+//   height: 100px;
+//   width: 100%;
+
+//   ${props =>
+//     props.scroll > 0 &&
+//     css`
+//       /* background-color: rgba(74, 138, 104, ${props.scroll * (1 / 400)}); */
+//       background-color: rgba(0, 61, 29, ${props.scroll * (1 / 400)});
+//       box-shadow: 0 6px 6px rgba(0, 0, 0, 0.25);
+//     `}
+
+//   ${props =>
+//     props.scroll < 200 &&
+//     css`
+//       height: calc(100px - ${props.scroll * (40 / 200)}px);
+//     `}
+
+//   ${props =>
+//     props.scroll >= 200 &&
+//     css`
+//       background-color: rgba(0, 61, 29, 1);
+//       height: 60px;
+//     `}
+// `
+
+// export const MenuContainer = styled.div`
+//   /* background-color: orange; */
+
+//   display: flex;
+//   justify-content: space-around;
+//   width: 75%;
+//   margin: 2px auto;
+// `
+
+// export const LogoLink = styled(Link)`
+//   /* background-color: yellow; */
+
+//   padding-top: 40px;
+//   width: 262px;
+//   height: 77px;
+// `
+
+// export const MenuGroup = styled.ul`
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+// `
+
+// export const MenuItem = styled.li`
+//   padding: 0 2px 0 2px;
+//   list-style: none;
+
+//   /* background-color: pink;
+//   border: 1px solid #fff; */
+// `
+
+// export const MenuLink = styled(Link)`
+//   font-family: "Raleway", sans-serif;
+//   font-weight: 300;
+//   font-size: 14px;
+//   line-height: 20px;
+//   text-align: center;
+//   letter-spacing: 1.5px;
+
+//   padding: 18px 30px;
+//   color: white;
+//   text-decoration: none;
+
+//   &:hover {
+//     font-weight: bold;
+//     border-bottom: 4px solid #8a6d37;
+//   }
+
+//   &.active {
+//     font-weight: bold;
+//     border-bottom: 4px solid #ffff;
+//   }
+// `
+
